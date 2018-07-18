028314f8b751d75e3b7d88e3929a715132ae710d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-18 01:35:56 -0400

    build basic videos serializer

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,24 @@
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :youtube_vid, :desc, :slides
+
+  def slides 
+    self.object.slides.map do |slide| 
+      sections = slide.sections.map do |section|
+        {id: section.id,
+         kind: section.kind,
+         order: section.order,
+         content: section.content,
+         desc: section.desc,
+         show_desc: section.show_desc
+        }
+      end 
+      {id: slide.id,
+       video_id: slide.video_id,
+       start: slide.start,
+       title: slide.title,
+       sections: sections
+      }
+    end 
+  end 
+  
 end
