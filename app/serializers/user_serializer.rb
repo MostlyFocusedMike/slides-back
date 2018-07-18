7672d462034ecb3ed6da68fb55f2bdb72ef130eb
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-18 01:22:31 -0400

    build basic users serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,12 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :username, :bio, :pic_link, :videos
+
+  def videos 
+    self.object.videos.map do |video|
+      {id: video.id,
+       youtube_vid: video.youtube_vid,
+       desc: video.desc
+      }
+    end
+  end 
+end
