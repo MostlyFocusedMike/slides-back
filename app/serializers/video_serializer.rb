06d032472b6848fc2927b26b7c1690ff3015d495
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-18 22:56:29 -0400

    add user to video serializer

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,5 +1,6 @@
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :youtube_vid, :desc, :topics, :slides, 
+  attributes :id, :youtube_vid, :desc, :user, :topics, :slides
+
   def topics 
     selected = self.object.topics.map do |topic|
       {id: topic.id, content: topic.content}
@@ -12,6 +13,7 @@ class VideoSerializer < ActiveModel::Serializer
       others: others
     }
   end 
+
   def slides 
     self.object.slides.map do |slide| 
       sections = slide.sections.map do |section|
@@ -23,13 +25,18 @@ class VideoSerializer < ActiveModel::Serializer
          show_desc: section.show_desc
         }
       end 
-      {id: slide.id,
-       video_id: slide.video_id,
-       start: slide.start,
-       title: slide.title,
-       sections: sections
+      return { 
+        id: slide.id,
+        video_id: slide.video_id,
+        start: slide.start,
+        title: slide.title,
+        sections: sections
       }
     end 
+  end 
+
+  def user 
+    {username: self.object.user.username, id: self.object.user.id}
   end 
   
 end
