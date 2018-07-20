7c42576d333bacefdfcad84a924f4fb43c3048c1
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-20 11:00:24 -0400

    build basic user create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,17 @@ class UsersController < ApplicationController
   end 
 
   def create 
-
+    @user = User.create(user_params)
+    if @user.save
+      render json: @user
+    else 
+      render json: @user.errors.messages 
+    end 
   end 
 
   private 
     def user_params
-      params.require(:user).permit(:username, :email, :bio, :pic_link, :password)
+      params.require(:user).permit(:username, :email, :bio, :pic_link, :password, :password_confirmation)
     end 
 
     def set_user 
