0ae4f301fbfa85a65af03ae10e3278685f753fa8
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-20 14:20:18 -0400

    load and give routes for JWT token

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :create, :update, :show]
   resources :videos, only: [:index, :create, :update, :show]
+
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
+    end
+  end
+
 end
