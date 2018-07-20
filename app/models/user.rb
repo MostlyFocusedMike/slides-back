e6dcd53471ddb9b621d9eb1d62ff0e1d2558e60b
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-20 11:03:56 -0400

    add basic validations to test error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :questions 
   has_many :questioned_videos, through: :questions, source: :video
   has_secure_password
+  validates :username, :email, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true 
 end
